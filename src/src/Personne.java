4c6027533cc0aed9fdafd4a0aaea6f7ebb376133
Vicolet <victor.nicolet@heig-vd.ch>
2024-10-31 15:30:47 +0100

    added Personne class

--- src/src/Personne.java
+++ src/src/Personne.java
@@ -1,2 +1,13 @@
-public class Personne {
+class Personne {
+    String nom;
+    String prenom;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String toString(){
+        return prenom + " " + nom;
+    }
 }
